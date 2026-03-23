42a685a117f3f9a7a989743b0d6beaa370f33bfe
lowkey2556 <lokpradeep255@gmail.com>
2026-03-23 09:22:10 +0530

    UC1: Display Consist Summary

--- src/Train.java
+++ src/Train.java
@@ -1,9 +1,13 @@
-public class Train{
-    public static void main(String[] args){
-        System.out.println("Train Consist Management App");
-        System.out.println("\nTrain initialized successfully...\n");
-        System.out.println("Initial Bogie Count : 0");
-        System.out.println("Current Train Consist : []\n");
-        System.out.println("System ready for operations");
+import java.util.ArrayList;
+public class Train {
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("========================================\n");
+        ArrayList<String> trainConsist = new ArrayList<>();
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial Bogie Count : " + trainConsist.size());
+        System.out.println("Current Train Consist : " + trainConsist + "\n");
+        System.out.println("System ready for operations...");
     }
 }
